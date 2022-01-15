614dbe6238c77b02182a9afff644e8154881f908
shivin01 <shivin.agarwal15@gmail.com>
2022-01-15 20:41:32 +0530

    Added more changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,25 @@
-// import logo from './logo.svg';
 import Button  from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Stack from 'react-bootstrap/Stack';
+import Row from 'react-bootstrap/Row';
 import { Link } from "react-router-dom";
-
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Container fluid>
+    {/* <Stack direction="horizontal" > */}
+    <Row>
+      <Col>
         <p> Welcome to Edith </p>
+        </Col>            
+      <Col>
         <Button as={Link as any} to='/login' variant="primary">Login</Button> {' '} <Button as={Link as any} to='/signup' variant="secondary">Sign Up</Button>
-      </header>
-    </div>
+      </Col>
+    </Row>
+    {/* </Stack> */}
+    </Container>
   );
 }
 
