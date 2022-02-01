c852b8f615641aadb165fa46be440196fd4ddfd8
shivin01 <shivin.agarwal15@gmail.com>
2022-02-01 23:49:16 +0530

    Bug fix added
    Signed-off-by: shivin01 <shivin.agarwal15@gmail.com>

--- edith_models/models/client.py
+++ edith_models/models/client.py
@@ -8,4 +8,4 @@ class Client(Base):
     registered_name = models.CharField(unique=True, max_length=Base.MAX_LENGTH_MEDIUM)
     address = models.TextField(null=True, blank=True)
     leave_count = models.IntegerField(default=0)
-    notice_period_count = models.IntegerField(default-0)
+    notice_period_count = models.IntegerField(default=0)
